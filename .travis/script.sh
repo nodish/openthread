e71a1ad9fbe782101e7de68fab09670f37931750
Yakun Xu <xyk@google.com>
2018-04-29 17:17:38 +0800

    increase jobs

--- .travis/script.sh
+++ .travis/script.sh
@@ -430,7 +430,7 @@ set -x
     make -f examples/Makefile-posix || die
     mv output $NCP_OUTPUT_DIR
     rm -rf build/*
-    NCP_FILE="$(pwd)/$(ls $NCP_OUTPUT_DIR/*/bin/ot-ncp-radio)" COVERAGE=1 RADIO=ncp BuildJobs=5 make -f examples/Makefile-posix check || die
+    NCP_FILE="$(pwd)/$(ls $NCP_OUTPUT_DIR/*/bin/ot-ncp-radio)" COVERAGE=1 RADIO=ncp make -f examples/Makefile-posix check || die
 }
 
 [ $BUILD_TARGET != posix-mtd ] || {
